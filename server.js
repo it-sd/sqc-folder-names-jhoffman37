cdf8310d6cb6c3e81e980ae429d0eccfdac4466c
jhoffman37 <123509441+jhoffman37@users.noreply.github.com>
2023-02-15 13:37:28 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 const newFolderName = function (folders) {
-
+if (folders == null) {
+return "New Folder"
 }
+  else  {
+  for (i = 0; i != folders.length; i++) {
+    if (i == folders.length) {
+      return "New Folder (" + (i + 1) + ")"
+}
+console.log(newFolderName())
