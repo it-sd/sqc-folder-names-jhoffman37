1df9e88ff8fd9b0ff46fbfacaedc6c1a8f6f7172
jhoffman37 <jhoffman37@student.cvtc.edu>
2023-03-08 10:29:39 -0600

    lint to fix errors

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const newFolderName = function (folders) {
-    const checks = 500
-    if (!folders.includes('New Folder')) {
-        return 'New Folder'
-    } else {
-        for (let i = 2; i < checks; i++) {
-            if (!folders.includes(`New Folder (${i})`)) {
-                return `New Folder (${i})`
-            }
-        }
+  const checks = 500
+  if (!folders.includes('New Folder')) {
+    return 'New Folder'
+  } else {
+    for (let i = 2; i < checks; i++) {
+      if (!folders.includes(`New Folder (${i})`)) {
+        return `New Folder (${i})`
+      }
     }
+  }
 }
 module.exports = { newFolderName }
